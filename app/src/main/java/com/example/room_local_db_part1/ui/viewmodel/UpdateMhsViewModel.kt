70d35e102fe6deaaf1e76669227697572e052f4c
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-17 00:16:34 +0700

    Implementasikan pengelolaan state dan logika pembaruan untuk data Mahasiswa pada ViewModel

--- app/src/main/java/com/example/room_local_db_part1/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/room_local_db_part1/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,12 +1,44 @@
 package com.example.room_local_db_part1.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.room_local_db_part1.data.entity.Mahasiswa
 import com.example.room_local_db_part1.repository.RepositoryMhs
+import com.example.room_local_db_part1.ui.navigation.DestinasiUpdate
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class updateMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel() {
 
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        updateUIState = updateUIState.copy(
+            mahasiswaEvent = mahasiswaEvent,
+        )
+    }
 }
+
+fun Mahasiswa.toUIStateMhs() : MhsUIState = MhsUIState (
+    mahasiswaEvent = this.toDetailUiEvent(),
+)
