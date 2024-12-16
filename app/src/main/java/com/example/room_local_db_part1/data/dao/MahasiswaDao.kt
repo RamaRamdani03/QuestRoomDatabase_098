dcfc65d500d9c7439130a11406a1a654c4a27688
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-16 20:00:37 +0700

    Metode update digunakan untuk memperbarui informasi mahasiswa yang sudah ada didatabase

--- app/src/main/java/com/example/room_local_db_part1/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/room_local_db_part1/data/dao/MahasiswaDao.kt
@@ -2,10 +2,14 @@ package com.example.room_local_db_part1.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Update
 import com.example.room_local_db_part1.data.entity.Mahasiswa
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
