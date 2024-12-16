f7f5d80a990d5d063f27d7887443ff80ef06cc64
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-16 19:54:45 +0700

    Implementasi awal untuk interface MahasiswaDao

--- app/src/main/java/com/example/room_local_db_part1/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/room_local_db_part1/data/dao/MahasiswaDao.kt
@@ -1,13 +1,8 @@
 package com.example.room_local_db_part1.data.dao
 
 import androidx.room.Dao
-import androidx.room.Insert
-import com.example.room_local_db_part1.data.entity.Mahasiswa
 
 @Dao
 interface MahasiswaDao {
-    @Insert
-    suspend fun insertMahasiswa(
-        mahasiswa: Mahasiswa
-    )
+
 }
