7e8456045725911419b60d205b66fe5c53fff662
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-16 19:56:48 +0700

    Menambahkan fungsi insertMahasiswa ke dalam interface MahasiswaDao

--- app/src/main/java/com/example/room_local_db_part1/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/room_local_db_part1/data/dao/MahasiswaDao.kt
@@ -1,8 +1,11 @@
 package com.example.room_local_db_part1.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import com.example.room_local_db_part1.data.entity.Mahasiswa
 
 @Dao
 interface MahasiswaDao {
-
+    @Insert
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
 }
