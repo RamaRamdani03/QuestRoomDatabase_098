ed1bc4a03efe67b103a5c1da397ab6c7c24d2a1f
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-16 23:13:00 +0700

    Menambahkan objek DestinasiHome yang mengimplementasikan AlamatNavigasi

--- app/src/main/java/com/example/room_local_db_part1/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/room_local_db_part1/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,7 @@ package com.example.room_local_db_part1.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
