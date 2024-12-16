ce879fc5857c628056a766ed143d4e3c16f8edef
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-16 23:32:00 +0700

    Menambahkan alur data StateFlow untuk mengelola Ui State di MahasiswaHomeViewModel

--- app/src/main/java/com/example/room_local_db_part1/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/room_local_db_part1/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,10 +1,48 @@
 package com.example.room_local_db_part1.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.room_local_db_part1.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class MahasiswaHomeViewModel (
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel() {
+
+    val homeUIState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState (
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true
+            )
+        )
 
 }
