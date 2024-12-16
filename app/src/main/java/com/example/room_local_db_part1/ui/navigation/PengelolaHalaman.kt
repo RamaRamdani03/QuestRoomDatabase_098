722cb99d1694d092f0fe17ab5306d3c77f480b90
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-17 04:30:24 +0700

    ``` Menambahkan navigasi ke halaman HomeMhsView dengan dukungan navigasi ke Detail dan Insert.

--- app/src/main/java/com/example/room_local_db_part1/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/room_local_db_part1/ui/navigation/PengelolaHalaman.kt
@@ -3,8 +3,16 @@ package com.example.room_local_db_part1.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.room_local_db_part1.ui.view.mahasiswa.DestinasiInsert
+import com.example.room_local_db_part1.ui.view.mahasiswa.DetailMhsView
+import com.example.room_local_db_part1.ui.view.mahasiswa.HomeMhsView
+import com.example.room_local_db_part1.ui.view.mahasiswa.InsertMhsView
+import com.example.room_local_db_part1.ui.view.mahasiswa.UpdateMhsView
 
 @Composable
 fun PengelolaHalaman(
@@ -15,6 +23,19 @@ fun PengelolaHalaman(
         navController = navController,
         startDestination = DestinasiHome.route
     ) {
-
+        composable(
+            route = DestinasiHome.route
+        ) {
+            HomeMhsView(
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                    println("PengelolaHalaman: nim = $nim")
+                },
+                onAddMhs = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+                modifier = Modifier
+            )
+        }
     }
 }
