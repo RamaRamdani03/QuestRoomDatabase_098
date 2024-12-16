ec660354cdbbb28d738aea962c9c7de161291c8c
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-17 02:01:26 +0700

    Implementasikan UI BodyHomeMhsView dengan indikator loading, penanganan error, dan tampilan kosong

--- app/src/main/java/com/example/room_local_db_part1/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/room_local_db_part1/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,21 +1,47 @@
 package com.example.room_local_db_part1.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.room_local_db_part1.data.entity.Mahasiswa
 import com.example.room_local_db_part1.ui.costumwidget.TopAppBar
+import com.example.room_local_db_part1.ui.viewmodel.HomeUiState
 import com.example.room_local_db_part1.ui.viewmodel.MahasiswaHomeViewModel
 import com.example.room_local_db_part1.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
 
 @Composable
 fun HomeMhsView(
@@ -54,5 +80,65 @@ fun HomeMhsView(
             },
             modifier = Modifier.padding(innerPadding)
         )
+    }
+}
+
+@Composable
+fun BodyHomeMhsView (
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when {
+        homeUiState.isLoading -> {
+            //menampilkan indikator loading
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            //menampilkan pesan error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let{ message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message) //show snackbar
+                    }
+                }
+            }
+        }
+
+        homeUiState.listMhs.isEmpty() -> {
+            //menampilkan pesan jika data kosong
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text (
+                    text = "Tidak ada data Mahasiswa. ",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            //menampilkan daftar mahasiswa
+            ListMahasiswa(
+                listMhs =  homeUiState.listMhs,
+                onClick = {
+                    onClick(it)
+                    println(it)
+                },
+                modifier = modifier
+            )
+        }
     }
 }
