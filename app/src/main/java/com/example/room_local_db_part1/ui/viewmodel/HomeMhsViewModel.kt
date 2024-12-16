47ff2e606d51f2985869fbbb25e792cc3c50c5d9
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-16 23:24:27 +0700

    Menambahkan kelas MahasiswaHomeViewModel untuk mengelola Ui

---
+++ app/src/main/java/com/example/room_local_db_part1/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.room_local_db_part1.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.room_local_db_part1.repository.RepositoryMhs
+
+class MahasiswaHomeViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+}
