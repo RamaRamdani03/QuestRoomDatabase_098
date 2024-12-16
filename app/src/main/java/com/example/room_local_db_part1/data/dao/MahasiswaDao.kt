412564d403c4292d347bb151e03893cb8c840725
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-16 20:04:38 +0700

    DeleteMahasiswa memiliki fungsi untuk menghapus data dari mahasiwa tertentu dari database, karena operasi ini bersifat I/O, metode ini ditandai dengan kata kunci "suspend" yang menunjukkan bahwa metode ini harus dijalankan dalam korutin

--- app/src/main/java/com/example/room_local_db_part1/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/room_local_db_part1/data/dao/MahasiswaDao.kt
@@ -1,6 +1,7 @@
 package com.example.room_local_db_part1.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Update
 import com.example.room_local_db_part1.data.entity.Mahasiswa
@@ -12,4 +13,7 @@ interface MahasiswaDao {
 
     @Update
     suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
 }
