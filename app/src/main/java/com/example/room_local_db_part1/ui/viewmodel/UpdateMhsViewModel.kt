38e761c63e17e95b782d82f9434f2530b54f126d
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-17 00:11:05 +0700

    Menambahkan viewModel untuk update data mahasiswa dengan menggunakan integrasi repository

---
+++ app/src/main/java/com/example/room_local_db_part1/ui/viewmodel/UpdateMhsViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.room_local_db_part1.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.room_local_db_part1.repository.RepositoryMhs
+
+class updateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+}
